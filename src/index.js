6fd63207f4dc5cb4aad7c8ea3af7effc495a48c6
Troy Presley <troy.presley@gmail.com>
2022-05-13 19:40:08 -1000

    remove unneded dependency

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import { run } from '@cycle/run'
 import { withState } from '@cycle/state'
 import { makeDOMDriver } from '@cycle/dom'
-import localStorageDriver from './lib/localStorageDriver'
 import App from './app'
 
 const main = withState(App, 'STATE')
 
 const drivers = {
   DOM:   makeDOMDriver('#root'),
-  STORE: localStorageDriver,
 }
 
 run(main, drivers)
