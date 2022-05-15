3f2a54c0bb3900c084ba3132d5ed2e4ced4e2870
Troy Presley <troy.presley@gmail.com>
2022-05-14 21:42:45 -1000

    refactor for new component API

--- src/tile.jsx
+++ src/tile.jsx
@@ -5,47 +5,49 @@ import delay from 'xstream/extra/delay'
 import { component, ABORT } from 'cyclejs-component'
 import classes from './lib/classses'
 
-const model = {
-  HIDE: (state, data, next) => {
-    if (!state.deleted) return ABORT
-    setTimeout(_ => next('DELETE'), 1000)
-    return { id: state.id, value: 0, row: state.row, column: state.column, deleted: true, hidden: true }
+
+export default component({
+  name: 'TILE',
+
+  model: {
+    HIDE: (state, data, next) => {
+      if (!state.deleted) return ABORT
+      setTimeout(_ => next('DELETE'), 1000)
+      return { id: state.id, value: 0, row: state.row, column: state.column, deleted: true, hidden: true }
+    },
+    DELETE: (state) => undefined
   },
-  DELETE: (state) => undefined
-}
 
-function intent({ STATE, DOM }) {
-  const state$     = STATE.stream.filter(state => state.deleted && !state.hidden)
-  const animation$ = DOM.select('.tile').events('transitionend')
-  const hide$      = state$
-    .map(_ => {
-      return xs.merge(xs.of(null).compose(delay(500)), animation$).take(1)
-    })
-    .flatten()
+  intent: ({ STATE, DOM }) => {
+    const state$     = STATE.stream.filter(state => state.deleted && !state.hidden)
+    const animation$ = DOM.select('.tile').events('transitionend')
+    const hide$      = state$
+      .map(_ => {
+        return xs.merge(xs.of(null).compose(delay(500)), animation$).take(1)
+      })
+      .flatten()
 
-  return {
-    HIDE: hide$
+    return {
+      HIDE: hide$
+    }
+  },
+
+  view: ({ state }) => {
+    const { id, value, row, column, hidden } = state
+    const classNames = classes('tile', `tile-${ id }`, { 'new': !!state.new })
+    const log2val = Math.log2(state.value || 0)
+    const color   = Math.floor(((50 * log2val) / 11) + 30)
+    const style   = {
+      '--row': `       ${row}`,
+      '--col': `       ${column}`,
+      '--tile-color': `${color}%`,
+      display: !!hidden ? 'none' : 'inherit'
+    }
+
+    return (
+      <div className={ classNames } id={ `tile-${ id }` } style={ style }>
+        { value }
+      </div>
+    )
   }
-}
-
-function view({ state }) {
-  const { id, value, row, column, hidden } = state
-  const classNames = classes('tile', `tile-${ id }`, { 'new': !!state.new })
-  const log2val = Math.log2(state.value || 0)
-  const color   = Math.floor(((50 * log2val) / 11) + 30)
-  const style   = {
-    '--row': `       ${row}`,
-    '--col': `       ${column}`,
-    '--tile-color': `${color}%`,
-    display: !!hidden ? 'none' : 'inherit'
-  }
-
-  return (
-    <div className={ classNames } id={ `tile-${ id }` } style={ style }>
-      { value }
-    </div>
-  )
-}
-
-
-export default component({ name: 'TILE', model, intent, view })
+})
